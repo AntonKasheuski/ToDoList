3df9f477665240f94c94dfd6c38756148128eb0b
Anton Kasheuski <kascheikinen@gmail.com>
2022-01-26 17:04:01 +0300

    Lesson 4 tuesday - 1

--- src/store/todolists-reducer.ts
+++ src/store/todolists-reducer.ts
@@ -5,25 +5,24 @@ export type RemoveTodoListAT = {
     type: "REMOVE-TODOLIST"
     id: string
 }
-
 export type AddTodoListAT = {
     type: "ADD-TODOLIST"
     title: string
 }
-
 export type ChangeTodoListTitleAT = {
     type: "CHANGE-TODOLIST-TITLE"
     id: string
     title: string
 }
-
 export type ChangeTodoListFilterAT = {
     type: "CHANGE-TODOLIST-FILTER"
     id: string
     filter: FilterValuesType
 }
-
-export type ActionType = RemoveTodoListAT | AddTodoListAT | ChangeTodoListTitleAT | ChangeTodoListFilterAT
+export type ActionType = RemoveTodoListAT
+    | AddTodoListAT
+    | ChangeTodoListTitleAT
+    | ChangeTodoListFilterAT
 
 export const todolistsReducer = (todoLists: Array<TodoListType>, action: ActionType): Array<TodoListType> => {
     switch (action.type) {
@@ -44,11 +43,9 @@ export const todolistsReducer = (todoLists: Array<TodoListType>, action: ActionT
 export const RemoveTodoListAC = (id: string): RemoveTodoListAT => {
     return {type: "REMOVE-TODOLIST", id}
 }
-
 export const AddTodoListAC = (title: string): AddTodoListAT => {
     return {type: "ADD-TODOLIST", title}
 }
-
 export const ChangeTodoListTitleAC = (id: string, title: string): ChangeTodoListTitleAT => {
     return {
         type: "CHANGE-TODOLIST-TITLE",
@@ -56,7 +53,6 @@ export const ChangeTodoListTitleAC = (id: string, title: string): ChangeTodoList
         title
     }
 }
-
 export const ChangeTodoListFilterAC = (id: string, filter: FilterValuesType): ChangeTodoListFilterAT => {
     return {
         type: "CHANGE-TODOLIST-FILTER",
